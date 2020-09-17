927d6d2ebb804c9751b29c372a77472b05d60ab3
David Moraes <investtraderrs@gmail.com>
2020-09-17 12:19:06 -0300

    Conceito de Vetores utilizando as Referencias

---
+++ VetoresUtilizandoReferencias/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresUtilizandoReferencias
+{
+    class Produto
+    {
+        public string Nome { get; private set; }
+        public int Quantidade { get; private set; }
+        public double Preco { get; private set; }
+
+        public Produto(string nome, int quantidade, double preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public double Total
+        {
+            get { return Quantidade * Preco; }
+        }
+
+        public override string ToString()
+        {
+            return "Produto: "
+                + Nome
+                + ", Quantidade: "
+                + Quantidade
+                + ", Preço Unitário: R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Valor Total: R$ "
+                + Total.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ VetoresUtilizandoReferencias/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresUtilizandoReferencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n;
+            Console.Write("Quantos Produtos serão cadastrados? ");
+            n = int.Parse(Console.ReadLine());
+
+            Produto[] vet = new Produto[n];
+
+            for (int i = 0; i<n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Digite os Dados do "+ (i+1) + "o produto: ");
+                Console.WriteLine();
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Quantidade: ");
+                int qtd = int.Parse(Console.ReadLine());
+                Console.Write("Preço: ");
+                double preco = double.Parse(Console.ReadLine(), CI);
+
+                vet[i] = new Produto(nome, qtd, preco);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Produtos Cadastrados: ");
+            for(int i =0; i<n; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            
+        }
+    }
+}
